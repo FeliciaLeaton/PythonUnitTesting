0324b27fa730c96814995efcde8aaecd96de8301
Connor Redmond <cdrm9t@umsystem.edu>
2021-03-19 17:07:40 +0000

    project3.py edited online with Bitbucket

--- project3.py
+++ project3.py
@@ -48,108 +48,117 @@ def getData(time_series, symbol, api_key):
         apistring += "&interval=30min"
     apistring +="&apikey=" + api_key
     
-    apistring = "https://www.alphavatage.co/query?function="
-    print(apistring)
+    data = requests.get(apistring)
+    return data.text
+    
+do_program = True
+while (do_program):
 
-print("Stock Data Visualizer\n======================")
+    print("Stock Data Visualizer\n======================")
 
-symbol = input("\nEnter the stock symbol are looking for: ")
-#check and see if symbol exist > error handling
-#probably just hit the api with a constant time_series value and input the requested symbol
-#see if we get a good respone
+    symbol = input("\nEnter the stock symbol are looking for: ")
+    #check and see if symbol exist > error handling
+    #probably just hit the api with a constant time_series value and input the requested symbol
+    #see if we get a good respone
 
 
 
-print("\nChart Types\n==================\n1. Bar\n2. Line")
-chart_type = input("Enter the chart type you want (1 , 2): ")
-# make sure it's 1 or 2, while loop > error handling
-while (chart_type != "1" and chart_type != "2"):
-    print("Enter a 1 or 2 for the chart type")
+    print("\nChart Types\n==================\n1. Bar\n2. Line")
     chart_type = input("Enter the chart type you want (1 , 2): ")
+    # make sure it's 1 or 2, while loop > error handling
+    while (chart_type != "1" and chart_type != "2"):
+        print("Enter a 1 or 2 for the chart type")
+        chart_type = input("Enter the chart type you want (1 , 2): ")
 
-if (chart_type == "1"):
-    #implement function to get bar chart > API integration
-    print("1")
-if (chart_type == "2"):
-    #implement function to get line chart > API inegration
-    print("2")
+    if (chart_type == "1"):
+        #implement function to get bar chart > API integration
+        print("1")
+    if (chart_type == "2"):
+        #implement function to get line chart > API inegration
+        print("2")
 
 
 
 
-print("\nSelect the Time Series of chart you want to Generate\n=================================================================")
-print("\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly")
-time_series = input("Enter the time series option (1, 2, 3, 4): ")
-# error check option picked > error handling
-while time_series not in ('1','2','3','4'):
-    print("Enter a 1, 2, 3, or 4 for the time series option")
+    print("\nSelect the Time Series of chart you want to Generate\n=================================================================")
+    print("\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly")
     time_series = input("Enter the time series option (1, 2, 3, 4): ")
+    # error check option picked > error handling
+    while time_series not in ('1','2','3','4'):
+        print("Enter a 1, 2, 3, or 4 for the time series option")
+        time_series = input("Enter the time series option (1, 2, 3, 4): ")
 
-if (time_series == '1'):
-    #implement function to get bar chart > API integration
-    print("1")
-if (time_series == '2'):
-    #implement function to get line chart > API inegration
-    print("2")
-if (time_series == '3'):
-    #implement function to get bar chart > API integration
-    print("3")
-if (time_series == '4'):
-    #implement function to get line chart > API inegration
-    print("4")
-
-
-
-
-
-# start_date = input("Enter the start Date (YYYY-MM-DD):")
-# error handling > check valid date in YYYY-MM-DD
+    if (time_series == '1'):
+        #implement function to get bar chart > API integration
+        print("1")
+    if (time_series == '2'):
+        #implement function to get line chart > API inegration
+        print("2")
+    if (time_series == '3'):
+        #implement function to get bar chart > API integration
+        print("3")
+    if (time_series == '4'):
+        #implement function to get line chart > API inegration
+        print("4")
 
 
-valid = False
-while not valid:
-    start_date = input("Enter the start Date (YYYY-MM-DD): ")
-    try:
-        time.strptime(start_date, "%Y-%m-%d")
-        valid = True
-        break
-    except ValueError:
-        print("Enter a valid start date")
-        valid = False
 
-print(start_date)
 
-# end_date = input("Enter the end Date (YYYY-MM-DD):")
-# error hadling > check valid date in YYYY-MM-DD and that it is after the start date
 
-valid = False
-while not valid:
-    end_date = input("Enter the end Date (YYYY-MM-DD): ")
-    try:
-        time.strptime(end_date, "%Y-%m-%d")
-        if time.strptime(end_date, "%Y-%m-%d") > time.strptime(start_date, "%Y-%m-%d"):
+    # start_date = input("Enter the start Date (YYYY-MM-DD):")
+    # error handling > check valid date in YYYY-MM-DD
+
+
+    valid = False
+    while not valid:
+        start_date = input("Enter the start Date (YYYY-MM-DD): ")
+        try:
+            time.strptime(start_date, "%Y-%m-%d")
             valid = True
-        else:
-            print("End date must be later than start date.")
+            break
+        except ValueError:
+            print("Enter a valid start date")
             valid = False
-    except ValueError:
-        print("Enter a valid end date")
-        valid = False
 
-print(end_date)
+    print(start_date)
+
+    # end_date = input("Enter the end Date (YYYY-MM-DD):")
+    # error hadling > check valid date in YYYY-MM-DD and that it is after the start date
+
+    valid = False
+    while not valid:
+        end_date = input("Enter the end Date (YYYY-MM-DD): ")
+        try:
+            time.strptime(end_date, "%Y-%m-%d")
+            if time.strptime(end_date, "%Y-%m-%d") > time.strptime(start_date, "%Y-%m-%d"):
+                valid = True
+            else:
+                print("End date must be later than start date.")
+                valid = False
+        except ValueError:
+            print("Enter a valid end date")
+            valid = False
+
+    print(end_date)
 
 
-getData(time_series,symbol,api_key)
+    json = getData(time_series,symbol,api_key)
+    print(json)
+    
+    
+    #parse out json data that comes back from API
+
+    #use that data to create graph data 
+    #create lmxl file with graph data
+
+    #send lxml to http and open file
 
 
-
-#parse out json data that comes back from API
-
-#use that data to create graph data 
-#create lmxl file with graph data
-
-#send lxml to http and open file
-
-
-
-#after everything is pretty much toss it all in a while loop so user can re run a visualizaiton
+    #after everything is pretty much toss it all in a while loop so user can re run a visualizaiton
+    
+    #after everything is pretty much toss it all in a while loop so user can re run a visualizaiton
+    an = str(input("Do you want to check another stock?"))
+    if (an == 'y' or an == 'Y'):
+        continue
+    else: 
+        do_program = False
